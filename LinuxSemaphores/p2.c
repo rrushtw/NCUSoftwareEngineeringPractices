94db6588fd320cdf66308a6c5de29bf32e601c08
Sung-Ai <rrushtw@gmail.com>
2022-10-03 15:39:05 +0800

    Finished. It turns wrong sometimes.

--- LinuxSemaphores/p2.c
+++ LinuxSemaphores/p2.c
@@ -5,12 +5,23 @@ main()
 {
     int i = 0;
     // *** Please insert proper semaphore initialization here
+
+    int sem1 = get_sem(".", 'A');
+    int sem2 = get_sem(".", 'B');
+    int sem3 = get_sem(".", 'C');
+
     do
     {
         // *** this is where you should place semaphore
 
+        P(sem2); // save itself
+        P(sem2); // save itself
+
         printf("P222222222 is here\n");
         i++;
+
+        V(sem3); // call back p3
+        V(sem3); // call back p3
 
         // *** this is where you should place semaphore
 
--- LinuxSemaphores/p3.c
+++ LinuxSemaphores/p3.c
@@ -5,9 +5,19 @@ main()
 {
     int i = 0;
     // *** Please insert proper semaphore initialization here
+
+    int sem1 = get_sem(".", 'A');
+    int sem2 = get_sem(".", 'B');
+    int sem3 = get_sem(".", 'C');
+
     do
     {
         // *** this is where you should place semaphore
+
+        V(sem1); // call p1
+        V(sem2); // call p2
+
+        P(sem3); // save itself for p2 calls
 
         printf("P3333333 is here\n");
         i++;
